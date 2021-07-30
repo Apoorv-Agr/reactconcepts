cf75735b07a9a49508a3a64fe36063691c976c4d
Apoorv <agrawal.av27@gmail.com>
2021-07-31 01:09:56 +0530

    functional Component commit 1

---
+++ src/FunctionalComponent/FunctionalComponentOne.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FunctionalComponentOne() {
+    return (
+        <div>
+            My First functional Component
+        </div>
+    )
+}
+
+export default FunctionalComponentOne;
