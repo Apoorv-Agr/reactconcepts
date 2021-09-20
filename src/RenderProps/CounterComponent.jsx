d5aa43a572779f4c03da92167b139dd9cb9bd17e
Apoorv <agrawal.av27@gmail.com>
2021-09-20 18:39:45 +0530

    Putting comments in the code of render props

--- src/RenderProps/CounterComponent.jsx
+++ src/RenderProps/CounterComponent.jsx
@@ -1,16 +1,15 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const CounterComponent = (props) => {
-    
-    const [counter,setCounter] = useState(0);
-    const increaseCounter = () =>{
-        setCounter(counter+1);
-    }
-    return (
-        <div>
-            {props.children(counter,increaseCounter)}
-            {/* {props.children(counter,increaseCounter)} */}
-        </div>
-    )
-}
+  const [counter, setCounter] = useState(0);
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  };
+  return (
+    <div>
+      {props.children(counter, increaseCounter)}
+      {/* {props.render(counter,increaseCounter)} */}
+    </div>
+  );
+};
 export default CounterComponent;
